21c1a25192e21826af9615d9936266216a9f333d
Hans Sjunnesson <hans.sjunnesson@monterosa.se>
2011-12-02 13:51:24 +0100

    Custom

--- hs-custom.el
+++ hs-custom.el
@@ -1,3 +1,5 @@
+(setq custom-file "~/.emacs.d./hs-custom.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -9,6 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(highlight ((t nil))))
 
 (provide 'hs-custom)
+
