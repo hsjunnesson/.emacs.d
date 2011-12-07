c5694dd9439c067ffd1c9ef0a9426256348175fc
Hans Sjunnesson <hans.sjunnesson@monterosa.se>
2011-12-07 15:45:25 +0100

    Custom

--- hs-custom.el
+++ hs-custom.el
@@ -12,7 +12,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (quote ("~/Dropbox/work.org")))
- '(org-clock-idle-time 10))
+ '(org-clock-idle-time 10)
+ '(wolfram-alpha-app-id "2JTYAL-2XAYU3XVE3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
